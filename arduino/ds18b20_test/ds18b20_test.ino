42062b2b47b0921a0e05d75916de9b475278b501
onsar <oscar.puyal@gmail.com>
2016-11-08 09:19:26 +0100

    [arduino]mesnajes json, borrar alarma temeperatura

--- arduino/ds18b20_test/ds18b20_test.ino
+++ arduino/ds18b20_test/ds18b20_test.ino
@@ -1,3 +1,11 @@
+/*
+ * 
+ * v01 2016-11-08
+ * Envio formato json
+ * Borrado alamras temperatura
+ * 
+ */
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "temperature_sensor_configuration.h"
@@ -5,17 +13,9 @@
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 
-
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-
-// arrays to hold device addresses
-// DeviceAddress insideThermometer, outsideThermometer;
 DeviceAddress tempDeviceAddress;
 
+// Number of temperature devices found
 int numberOfDevices; // Number of temperature devices found
 
 //DeviceAddress
@@ -32,88 +32,84 @@ uint8_t deviceAddress_10[]=POSITION_10;
 uint8_t deviceAddress_11[]=POSITION_11;
 uint8_t deviceAddress_12[]=POSITION_12;
 
+bool compareAddress(DeviceAddress deviceAddress_c_1, DeviceAddress deviceAddress_c_2);
+void printAddress(DeviceAddress deviceAddress);
+
+// Function Prototypes
+void printTemperature(DeviceAddress deviceAddress);
+void printResoltion(DeviceAddress deviceAddress);
+void printData(DeviceAddress deviceAddress,int n, bool p_resolution);
+void temperatureSensorMessage();
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
 void setup(void)
 {
-  // start serial port
+  // Start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.print("{\"maint_search_temp_sensor\":{");
 
   // Start up the library
   sensors.begin();
   
-  // locate devices on the bus
-  
-  Serial.println("Locating devices..."); 
-  numberOfDevices = sensors.getDeviceCount();
-  Serial.println("********************"); 
-  Serial.print("Found ");
-  Serial.print(numberOfDevices, DEC);
-  Serial.println(" devices.");
-  Serial.println("********************"); 
-  
+  // Locate devices on the bus
 
+  numberOfDevices = sensors.getDeviceCount();
+  Serial.print("\"devices_found\":\"YES\",");
+  Serial.print("\"num_devices:\":\"");
+  Serial.print(numberOfDevices, DEC);
+  Serial.print("\"");
   
-// *******************  Empieza el bucle
+  // Close mintenence, message
+  Serial.println("}}");  
 
 
   // Loop through each device, print out address and configuration
-  // reolution, alarms
-  
+  // Reolution alarms configuration
+  Serial.print("{\"maint_data_temp_sensor\":{"); 
   for(int i=0;i<numberOfDevices; i++)
-  {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i))
-    {
-        Serial.print("Found device ");
-        Serial.print(i, DEC);
-        Serial.print(" with address: ");
-        printAddress(tempDeviceAddress);
-        Serial.println();
-        
-        Serial.print("Setting resolution to ");
-        Serial.println(TEMPERATURE_PRECISION, DEC);
-        
         // set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
-        sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
-        
-        Serial.print("Resolution actually set to: ");
-        Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
-        Serial.println();
-        
-        Serial.println("current alarm setup: ");   
-        printAlarms(tempDeviceAddress);
-        Serial.println();
-        
-        Serial.println("Setting alarm temp...");
+    {
+        // Search the wire for address
+        if(sensors.getAddress(tempDeviceAddress,i))
+            {
+                sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+                printData(tempDeviceAddress,i,true); 
+                if (i < (numberOfDevices-1))Serial.print(",");
+            }
 
-        // alarm when temp is higher than 30C
-
-        sensors.setHighAlarmTemp(tempDeviceAddress, HIGH_TEMPERATURE_ALARM);
-    
-        // alarm when temp is lower than -10C
-        sensors.setLowAlarmTemp(tempDeviceAddress, LOW_TEMPERATURE_ALARM);
-        
-        Serial.println("NEW alarm temp...");   
-        printAlarms(tempDeviceAddress);
-        Serial.println();
-
-        
-    }
-  
-  else{
-    Serial.print("Found ghost device at ");
-    Serial.print(i, DEC);
-    Serial.print(" but could not detect address. Check power and cabling");
-  }
-  }
-  
+        else
+            {
+                Serial.print("Found ghost device at ");
+                Serial.print(i, DEC);
+                Serial.print(" but could not detect address. Check power and cabling");
+            }
+            
+    }  
+        // Close mintenence, message
+        Serial.println("}}"); 
 }
 
+
 /**************************************/
-/*********  FUNCIONES *****************/
+/*********   LOOP     *****************/
+/**************************************/
+
+void loop(void)
+{ 
+  temperatureSensorMessage();
+}
+
+
+/**************************************/
+/*********  FUNCTIONS *****************/
 /**************************************/
   
-/** Comparar direcciones ***/
+// Compare directions
 
 bool compareAddress(DeviceAddress deviceAddress_c_1, DeviceAddress deviceAddress_c_2){
   for(int i=0;i<7; i++){
@@ -123,7 +119,7 @@ bool compareAddress(DeviceAddress deviceAddress_c_1, DeviceAddress deviceAddress
 }
 
   
-/** imprimir deviceAddress ***/
+// Print deviceAddress
 
 void printAddress(DeviceAddress deviceAddress)
 {
@@ -135,41 +131,31 @@ void printAddress(DeviceAddress deviceAddress)
 }
 
 
-/** imprimir la temperatura de un sensor ***/ 
+// Print tempearture
 
 void printTemperature(DeviceAddress deviceAddress)
 {
   float tempC = sensors.getTempC(deviceAddress);
-  Serial.print("Temp C: ");
   Serial.print(tempC);
-  Serial.print(" Temp F: ");
-  Serial.print(DallasTemperature::toFahrenheit(tempC));
 }
 
-// function to print High and Low alarmas
-void printAlarms(uint8_t deviceAddress[])
+void printResoltion(DeviceAddress deviceAddress)
 {
-  char temp;
-  temp = sensors.getHighAlarmTemp(deviceAddress);
-  Serial.print("High Alarm: ");
-  Serial.print(temp, DEC);
-  Serial.print("C/");
-  Serial.print(DallasTemperature::toFahrenheit(temp));
-  Serial.print("F | Low Alarm: ");
-  temp = sensors.getLowAlarmTemp(deviceAddress);
-  Serial.print(temp, DEC);
-  Serial.print("C/");
-  Serial.print(DallasTemperature::toFahrenheit(temp));
-  Serial.print("F");
+  Serial.print(sensors.getResolution(deviceAddress), DEC); 
 }
 
-/** Imprimir la informacion principal de un dispositivo ***/
-
-void printData(DeviceAddress deviceAddress)
+//Main data print function
+void printData(DeviceAddress deviceAddress,int n, bool p_resolution)
 {
-  Serial.print("Device Address: ");
-  printAddress(deviceAddress);
+  Serial.print("\"sensor_");
+  Serial.print(n);
+  Serial.print("\":{");
   
+  Serial.print("\"id\":\"");
+  printAddress(deviceAddress);
+  Serial.print("\","); 
+  
+  Serial.print("\"name\":\"");
   // Incluye el nombre del sensor
   if(compareAddress(deviceAddress,deviceAddress_1)) {Serial.print(NAME_SENSOR_1);}
   else if(compareAddress(deviceAddress,deviceAddress_2))  {Serial.print(NAME_SENSOR_2);} 
@@ -184,42 +170,48 @@ void printData(DeviceAddress deviceAddress)
   else if(compareAddress(deviceAddress,deviceAddress_11)) {Serial.print(NAME_SENSOR_11);} 
   else if(compareAddress(deviceAddress,deviceAddress_12)) {Serial.print(NAME_SENSOR_12);}   
   else {Serial.print(" LOCALIZACION NO IDENTIFICADA"  ); }
+  Serial.print("\","); 
   
-  Serial.print(" ");
+  // Resolution data
+  if(p_resolution)
+    {
+        Serial.print("\"res\":\"");
+        printResoltion(deviceAddress);
+        Serial.print("\","); 
+    }
+    
+  // Temeperature data
+  Serial.print("\"temp_c\":\"");
   printTemperature(deviceAddress);
+  Serial.print("\"");
+  Serial.print("}"); // sensor close
   
 }
-// Imprime todos los datos incluidas las alarmas
-void checkAlarm(DeviceAddress deviceAddress)
-{
-  printData(deviceAddress); 
-  if (sensors.hasAlarm(deviceAddress))
-    {
-      Serial.print(" ALARM");
-    }
-  else 
-    {
-      Serial.print(" NO ALARM");
-    }
-  Serial.println();
+
+
+void temperatureSensorMessage(){
+    uint32_t time = millis();
+ //   Serial.print("{\"data_sensor\":{\"code\":\"50001\",\"number\":\"001\",\"name\":\"San Pablo\",\"time_0x\":\"12345678\",\"temperature_sensors\":{");    
+    Serial.print("{\"data_sensor\":{\"code\":\"50001\",\"number\":\"001\",\"name\":\"San Pablo\",\"time_0x\":\""); 
+    Serial.print(time); 
+    Serial.print("\",");
+    
+    // temperature sensor message
+
+    Serial.print("\"temperature_sensors\":{");
+ 
+    sensors.requestTemperatures();
+    for(int i=0;i<numberOfDevices; i++)
+        {
+            // Search the wire for address
+            if(sensors.getAddress(tempDeviceAddress, i))
+                {
+                    printData(tempDeviceAddress,i,false); 
+                    if (i < (numberOfDevices-1))Serial.print(",");
+                }
+            
+        }
+     // close temperature_sensors,data_sensor,message    
+    Serial.println("}}}");
+    
 }
-
-void loop(void)
-{ 
-  // CALL sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures();
-  Serial.println("DONE");
-   for(int i=0;i<numberOfDevices; i++)
-   {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i))
-    {
-    checkAlarm(tempDeviceAddress);   
-    Serial.println();
-    }
-   }
-
-}
-
