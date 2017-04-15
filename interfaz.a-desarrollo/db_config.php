6c2d6dc3f73ccf3a442f04d2a65af9d40a938d04
onsar <oscar.puyal@gmail.com>
2017-04-15 10:11:51 +0200

    menor

--- interfaz.a-desarrollo/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-$db_host = 'localhost'; //hostname
-$db_user = 'pandora_read'; // username
-$db_password = 'pandora2'; // password
-$db_name = 'pandora2'; //database name
-$conn = mysql_connect($db_host, $db_user, $db_password );
-mysql_select_db($db_name, $conn);
-?>
