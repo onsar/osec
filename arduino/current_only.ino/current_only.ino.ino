add3d46de607fd27c8dda340b62c93f352c59952
onsar <oscar.puyal@gmail.com>
2017-05-11 11:46:38 +0200

    [nodemcu]basico para medida de intensidad

---
+++ arduino/current_only.ino/current_only.ino.ino
@@ -0,0 +1,21 @@
+// EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
+
+#include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon1;                   // Create an instance
+
+void setup()
+{  
+  Serial.begin(115200);
+  
+  emon1.current(A0, 111.1);             // Current: input pin, calibration.
+}
+
+void loop()
+{
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+
+  Serial.print("**************** ");
+  Serial.print(Irms*230.0);         // Apparent power
+  Serial.print(" ");
+  Serial.println(Irms);          // Irms
+}
